d0076bd4281e63d35bfb762efa3a3cd74c0d8ca2
Amit Upreti <a.u.aua937@gmail.com>
2026-04-16 11:43:40 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let results = []
+        let cringe_hashset = new Set()
+        for(let i=0; i < strs.length; i++){
+            const curr_word = strs[i]
+            if(cringe_hashset.has(i)){
+                continue
+            }
+            let anagrams_curr_word = [curr_word]
+            
+            cringe_hashset.add(i)
+            for (let j=i+1; j < strs.length;j++){
+                const word_to_compare = strs[j]
+                if (this.isAnagram(curr_word, word_to_compare)){
+                    anagrams_curr_word.push(word_to_compare)
+                    // console.log('fuck yeah', anagrams_curr_word)
+                    cringe_hashset.add(j)
+                }
+            }
+            results.push(anagrams_curr_word)
+            
+        }
+
+        return results
+
+    }
+
+    isAnagram(str1, str2){
+        // console.log(str1.split("").sort(), str2.split("").sort(), JSON.stringify(str1.split("").sort()) == JSON.stringify(str2.split("").sort()))
+        return JSON.stringify(str1.split("").sort()) == JSON.stringify(str2.split("").sort())
+    }
+}
