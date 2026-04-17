8d299562e4213d4fa8585d6920ed7b1d27cf5f4c
Amit Upreti <a.u.aua937@gmail.com>
2026-04-17 07:48:33 -0400

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    reverse_str_2_sorted_str =  new Map()
+
+    groupAnagrams(strs) {
+        for (let str of strs){
+            let sorted_str  = str.split("").sort().join("")
+            if(this.reverse_str_2_sorted_str.has(sorted_str)){
+                this.reverse_str_2_sorted_str.get(sorted_str).push(str)
+            }
+            else{
+                this.reverse_str_2_sorted_str.set(sorted_str,[str])
+            }
+            
+
+        }
+
+        let results = []
+        for (const [key, value] of this.reverse_str_2_sorted_str) {
+            results.push(value)
+
+
+        }
+        // # damn
+        return results
+
+    }
+
+  
+}
